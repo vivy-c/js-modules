6deae124abb9d2553ae08810db24bf3ccb18f5c5
vivycahyani <101357509+vivycahyani@users.noreply.github.com>
2022-05-17 03:42:27 +0700

    module object

---
+++ scripts/10-module-object.js
@@ -0,0 +1,14 @@
+export const firstName = "Vivy";
+export const middleName = "Cahyani";
+export const lastName = "Ball";
+
+export function hello(){
+    console.info("Hello from Vivy");
+}
+
+export class Person {
+
+    constructor() {
+        this.name = "Vvy";
+    }
+}
