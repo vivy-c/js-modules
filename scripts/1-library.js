a4ea2854fedfa94396ea8b8ce35e37f10334614b
vivycahyani <101357509+vivycahyani@users.noreply.github.com>
2022-05-17 03:05:59 +0700

    js-module first commit

---
+++ scripts/1-library.js
@@ -0,0 +1,11 @@
+// library
+
+function sayHello(){
+    console.info(`Hello ${name}`);
+}
+
+function sayGoodBye(){
+    console.info(`Good Bye ${name}`);
+}
+
+const name = "Vivy";
---
+++ scripts/2-say.js
@@ -0,0 +1,10 @@
+// use js module
+export function sayHello(name) {
+    console.info(`Hello ${name}`);
+}
+
+export function sayGoodBye(name) {
+    console.info(`Good Bye ${name}`);
+}
+
+export const name = "Vivy"; 
